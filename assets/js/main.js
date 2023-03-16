3100fa039008ca57ccd460db685d61edd253129a
Fabrizio1919 <fabriziosalerno19@gmail.com>
2023-03-16 17:52:21 +0100

    Add strcture pari o dispari create two varibili and function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,27 +4,59 @@
 function palindroma() {
 
     let parolaUtente = prompt('Inserisci la parola');
-    let parolaReverse = '';
-
-
-    function risultato(parolaUtente, parolaReverse) {
-
-        for (let i = parolaUtente.length - 1; i >= 0; i--) {
-            parolaReverse += parolaUtente[i]
-        }
-        if (parolaReverse == parolaUtente) {
-            alert('La parola è Palindroma')
-        }
-        else {
-            alert('La parola non è Palindroma')
-        }
+    
+    let parolaReverse = invertiParola(parolaUtente);
+    
+    if (parolaReverse == parolaUtente) {
+        alert('La parola è Palindroma')
     }
-    risultato(parolaUtente, parolaReverse);
-   
+    else {
+        alert('La parola non è Palindroma')
+    }
 }
 
+function invertiParola(parola) {
+    let reverse = '';
+
+    for (let i = parola.length - 1; i >= 0; i--) {
+        reverse += parola[i]
+    }
+    return reverse;
+    
+}
 
 // Esercizio 2 " Pari e dispari"
 /* L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
+
+function gioca() {
+
+    let pariOdispari = prompt('Pari o Dispari ?');
+    let numeroDelUtente = parseInt(prompt('Inserisci un numero da 1 a 5'));
+    console.log(`Hai scelto ${pariOdispari}.`, `Il tuo numero scelto è ${numeroDelUtente}`);
+    
+    function risultato(pariDispari, numeroUtente) {
+        
+        let randomComputerNumber = Math.floor(Math.random() * 5 + 1);
+        console.log(`Il numero scelto dal computer è ${randomComputerNumber}`);
+        let somma = numeroUtente + randomComputerNumber;
+        console.log(`La somma è ${somma}`);
+
+        if (somma % 2 == 0 && pariDispari == 'pari') {
+            alert('Hai vinto');
+        
+        } else if (somma % 2 != 0 && pariDispari == 'dispari') {
+            alert('Hai vinto');
+            
+        } else {
+            alert('Hai Perso');
+        }
+
+    }
+
+    risultato(pariOdispari, numeroDelUtente);
+
+}
+
+
 
 
